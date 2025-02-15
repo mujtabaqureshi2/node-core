0a4d46427257d5a29d6501b29559ae290aa774b5
Mujtaba Qureshi <mmujtaba@emblemtechnologies.co>
2025-02-15 20:18:07 +0500

    os and filepath manipulation

---
+++ osDemo.js
@@ -0,0 +1,13 @@
+import os from 'os';
+
+// userInfo
+console.log(os.userInfo())
+
+// total memory
+console.log(os.totalmem())
+
+// free memory
+console.log(os.freemem())
+
+//cpus
+console.log(os.cpus())
